5568b8bd71fa06dbb7729a45fedb7aac863f8a8c
Mahmudjon Shukurov <sukurovmahmudzon15@gmail.com>
2024-05-05 21:36:07 +0300

    tg

---
+++ index.js
@@ -0,0 +1,13 @@
+let son1 = Number(prompt("Son1 ni kiriting: "));
+  
+let son2 = Number(prompt("Son2 ni kiriting: "));
+
+function kattami() {
+    if (son1 > son2) {
+        alert(son1 + " katta " + son2 +  " dan");
+    } else {
+        alert(son1 + " kichik " + son2 +  " dan");
+    }
+}
+
+kattami();
